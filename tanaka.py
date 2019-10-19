3514c53cc1faca2ba2c73ee491862931fb310b7a
tanawaii <ryotatnaka@gmai.com>
2019-10-20 02:26:26 +0900

    waii

--- tanaka.py
+++ tanaka.py
@@ -1,1 +1,12 @@
-print('hello')
+i = 1
+while i < 31:
+    if i % 15 == 0:
+        print("TanaWaii!")
+    elif i % 3 == 0:
+        print("Tana!")
+    elif i % 5 == 0:
+        print("Waii!")
+    else:
+        print(i)
+
+    i += 1
